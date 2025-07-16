8a1eb50ede013bf7eeac062b9dfb79ff8ea26c1e
cheevaher <cheevaher0101@gmail.com>
2025-07-16 09:41:47 +0700

    Update new code

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio_CheeVaher/", // ชื่อ repository
+  base: "/",  // หรือเอา base ออกเลยก็ได้ (default เป็น "/")
 })
